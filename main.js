7f1a2bd27877d3e0ece82cc6841e0461eb3f7a49
username <seanevanlee@gmail.com>
2022-11-14 13:31:10 -0800

    now with some audio and emojis selected

---
+++ main.js
@@ -0,0 +1,14 @@
+const emojiArray = [
+    '❄',
+    '🏂',
+    '⛄',
+    '🎅',
+    '🦌',
+    '🪨',
+];
+
+const christmasPresentsEls = {
+    slot1: document.querySelector('christmasPresent'),
+    slot2: document.querySelector('christmasPresent'),
+    slot3: document.querySelector('christmasPresent'),
+};
