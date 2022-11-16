b48491df276c2af572c76d6b1f8cb6ce59f8d7d6
username <seanevanlee@gmail.com>
2022-11-16 14:57:18 -0800

    now has cleaner if else with console log reflecting triples

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// a welcome alert upon opening the browser link
 alert("Christmas is on a Sunday this year\nBut for you, maybe it's early?");
 
-// random selector array
+// random selector array, 3 sounds like a manageable number
  const emojiArray = [
         '⛄',
         '🎅',
         '🦌',
     ]
+
+// create a way to get one of the 3 emojis, it doesn't matter which so it keeps the game fun
 function getRandom(){
     // const emojiArray = [
     //     '⛄',
@@ -24,11 +27,15 @@ function getRandom(){
 // one for all 3 santas = you win (Santa is the winning emoji)
 // one for not = you lose (3 of snowmen or 3 of reindeer get you nothing)
 // for loop to run through the game 5 times to try to win
-const christmasPresentsEls = {
-    slot1: document.querySelector('#present1'),
-    slot2: document.querySelector('#present2'),
-    slot3: document.querySelector('#present3'),
-};
+// const christmasPresentsEls = {
+//     slot1: document.querySelector('#present1'),
+//     slot2: document.querySelector('#present2'),
+//     slot3: document.querySelector('#present3'),
+// };
+
+let slot1 = document.querySelector('#present1')
+let slot2 = document.querySelector('#present2')
+let slot3 = document.querySelector('#present3')
 
 
 // next thing to do = Select each div with the div. Check the console as a reference
@@ -39,16 +46,26 @@ const christmasPresentsEls = {
 // once completed, you should see each square has an emoji on the webpage
 init() // this is calling a function
 function init(){
-christmasPresentsEls.slot1.innerText = getRandom()
-christmasPresentsEls.slot2.innerText = getRandom()
-christmasPresentsEls.slot3.innerText = getRandom()
+slot1.innerText = getRandom()
+slot2.innerText = getRandom()
+slot3.innerText = getRandom()
 }
 
 
 // create a play button that generates 3 at random. event listener to play button
-
-// document.getElementById("button").addEventListener("click here", getrandom());
-
+// the play function below tells the game to make a sound when the button is clicked as well as display different messages in the console.log. 
+function play() {
+    const audio = document.getElementById("audio");
+    audio.play();
+    if ((slot1.innerText === slot2.innerText) && (slot1.innerText === slot3.innerText) && (slot2.innerText === slot3.innerText)) {
+        // document.getElementById("") = "Santa thought you were nice this year"
+        console.log('Get your milk and cookies');
+    } else {
+        // display = "Please try again"
+        console.log('The Grinch stole Christmas! Please go again.');
+    }
+  }
+  
 const button = document.querySelector('button');
 button.addEventListener('click', playChristmas);
 
@@ -59,29 +76,15 @@ resetButton.addEventListener('click', init);
 
 function playChristmas(e){
     // console.log('Happy Holidays');
-    christmasPresentsEls.slot1.innerText = getRandom();
-    christmasPresentsEls.slot2.innerText = getRandom();
-    christmasPresentsEls.slot3.innerText = getRandom();
-
-// if (condition) {
-//     statement
-// }
-// else if (somethingElse) {
-//     statement2
-// }
-const winner = '🎅';
-let spins = {
-    player: 0,
-}
-
-if (christmasPresentsEls.slot1 === christmasPresentsEls.slot2 && christmasPresentsEls.slot1 === christmasPresentsEls.slot3 && christmasPresentsEls.slot2 === christmasPresentsEls.slot3) {
-    document.getElementById("") = "Santa thought you were nice this year"
-    console.log('Get your milk and cookies');
-} else {
-    display = "Please try again"
-    console.log('The Grinch stole Christmas! Please go again.');
-}
-spins.player += 1
+    slot1.innerText = getRandom();
+    slot2.innerText = getRandom();
+    slot3.innerText = getRandom();
 
 
 }
+
+
+
+// javascript to have everything start as blank (no emojis). emojis only pop up when the user hits play first.
+
+// dom lessions, function lessons
